d0a65056770e942829dae2376b49bec408f14f53
Yash sharma <yashsharmaofficially@gmail.com>
2026-06-05 13:59:59 +0530

    update

--- backend/programs/backend/src/instructions/initialize.rs
+++ backend/programs/backend/src/instructions/initialize.rs
@@ -1,14 +1,3 @@
-// use anchor_lang::prelude::*;
-
-// #[derive(Accounts)]
-// pub struct Initialize {}
-
-// pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-//     msg!("Greetings from: {:?}", ctx.program_id);
-//     Ok(())
-// }
-//
-
 use crate::state::Counter;
 use anchor_lang::prelude::*;
 #[derive(Accounts)]
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+const endPoint = clusterApiUrl("devnet");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ConnectionProvider endpoint={endPoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </StrictMode>,
 );
-
